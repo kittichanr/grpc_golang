04ecc8e9126079ae4457102f3ba6fb2132c2f214
kittichanr <petchkubbb@gmail.com>
2023-12-02 09:42:41 +0700

    gRPC Reflection with Evan Cli

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,6 +9,7 @@ import (
 	proto "github.com/kittichanr/pcbook/internal/gen/proto/pcbook/v1"
 	"github.com/kittichanr/pcbook/service"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -23,6 +24,8 @@ func main() {
 	laptopServer := service.NewLaptopServer(laptopStore, imageStore, ratingStore)
 	grpcServer := grpc.NewServer()
 	proto.RegisterLaptopServiceServer(grpcServer, laptopServer)
+	// Register reflection service on gRPC server.
+	reflection.Register(grpcServer)
 
 	address := fmt.Sprintf("0.0.0.0:%d", *port)
 	listener, err := net.Listen("tcp", address)
